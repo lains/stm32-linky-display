a389066304551694822f536c414a8e416bde8ee0
lains <383502+lains@users.noreply.github.com>
2023-01-25 13:04:16 +0100

    Fixing hexdump in test

--- tests/src/TicUnframer_tests.cpp
+++ tests/src/TicUnframer_tests.cpp
@@ -11,7 +11,7 @@ TEST_GROUP(TicUnframer_tests) {
 void onFrameDecode(const uint8_t* buf, size_t cnt) {
 	printf("Hello on buffer size %zu\n", cnt);
 	for (size_t pos = 0; pos < cnt; pos++) {
-		if (pos & 0x3 == 0) {
+		if ((pos & 0xf) == 0) {
 			printf("\n");
 		}
 		else {
@@ -24,26 +24,26 @@ void onFrameDecode(const uint8_t* buf, size_t cnt) {
 
 TEST(TicUnframer_tests, TicUnframer_test_basic) {
 	constexpr unsigned int BUFFERSIZE = 10 * 1024;
-	const char *inputFilename = "./continuous_linky_3P_historical_TIC_sample.bin";
-    FILE* ticFile = fopen(inputFilename, "rb" );
-    if (!ticFile) {
+	const char *inputFilename = "./samples/continuous_linky_3P_historical_TIC_sample.bin";
+	FILE* ticFile = fopen(inputFilename, "rb" );
+	if (!ticFile) {
 		FAILF("Error: could not open file %s", inputFilename);
 	}
 
-    uint8_t* buffer = new uint8_t[BUFFERSIZE];
+	uint8_t* buffer = new uint8_t[BUFFERSIZE];
 	TICUnframer tu(onFrameDecode);
-	int bytesRead;
+	int bytesRead = 0;
 	do {
 		int bytesRead = fread(buffer, sizeof(char), BUFFERSIZE, ticFile);
 		printf("Read %d bytes from dump\n", bytesRead);
 		tu.pushBytes(buffer, bytesRead);
 	}
-    while (bytesRead > 0 );
+	while (bytesRead > 0 );
 
 	delete[] buffer;
 
-    // Done and close.
-    fclose(ticFile);
+	// Done and close.
+	fclose(ticFile);
 }
 
 #ifndef USE_CPPUTEST
