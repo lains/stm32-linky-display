4afaf9605d2416fa7e48bc4b2593fb50284e9692
lains <383502+lains@users.noreply.github.com>
2023-01-25 15:25:57 +0100

    Adding unit tests

--- tests/src/TicUnframer_tests.cpp
+++ tests/src/TicUnframer_tests.cpp
@@ -1,11 +1,24 @@
 #include "TestHarness.h"
 #include <iostream>
 #include <iomanip>
+#include <vector>
 #include <stdint.h>
 
 #include "../src/TicUnframer.h"
 
 TEST_GROUP(TicUnframer_tests) {
+};
+
+class FrameDecoderStub {
+public:
+	FrameDecoderStub() : decodedFramesList() { }
+
+	void onDecodeCallback(const uint8_t* buf, size_t cnt) {
+		this->decodedFramesList.push_back(std::vector<uint8_t>(buf, buf+cnt));
+	}
+
+public:
+	std::vector<std::vector<uint8_t> > decodedFramesList;
 };
 
 void onFrameDecode(const uint8_t* buf, size_t cnt) {
@@ -20,6 +33,21 @@ void onFrameDecode(const uint8_t* buf, size_t cnt) {
 		printf("%02hhx", buf[pos]);
 	}
 	printf("\n");
+}
+
+TEST(TicUnframer_tests, TicUnframer_test_one_pure_stx_etx_frame_10bytes) {
+	uint8_t buffer[] = { TICUnframer::TIC_STX, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, TICUnframer::TIC_ETX };
+	FrameDecoderStub stub;
+	TICUnframer tu([&stub](const uint8_t* buf, size_t cnt) {
+		stub.onDecodeCallback(buf, cnt);
+	} );
+	tu.pushBytes(buffer, sizeof(buffer));
+	if (stub.decodedFramesList.size() != 1) {
+		FAILF("Wrong frame count");
+	}
+	if (stub.decodedFramesList[0] != std::vector<uint8_t>({0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39})) {
+		FAILF("Wrong frame decoded");
+	}
 }
 
 TEST(TicUnframer_tests, TicUnframer_test_basic) {
@@ -48,6 +76,7 @@ TEST(TicUnframer_tests, TicUnframer_test_basic) {
 
 #ifndef USE_CPPUTEST
 void runTicUnframerAllUnitTests() {
+	TicUnframer_test_one_pure_stx_etx_frame_10bytes();
 	TicUnframer_test_basic();
 }
 #endif	// USE_CPPUTEST
