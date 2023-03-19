bc82fb6c8067561af464b0a06576a0ae2f1bb275
lains <383502+lains@users.noreply.github.com>
2023-03-19 11:00:46 +0100

    Lining-up expected CRC with samples

--- tests/src/TicDatasetExtractor_tests.cpp
+++ tests/src/TicDatasetExtractor_tests.cpp
@@ -212,7 +212,7 @@ TEST(TicDatasetExtractor_tests, Chunked_sample_unframe_dsextract_historical_TIC)
 		if (stub.decodedDatasetList.size() != expectedTotalDatasetCount) { 
 			FAILF("When using chunk size %zu: Wrong dataset count: %zu, expected %zu\nDatasets received:\n%s", chunkSize, stub.decodedDatasetList.size(), expectedTotalDatasetCount, stub.toString().c_str());
 		}
-		char firstDatasetAsCString[] = "ADCO 056234673197 ,";
+		char firstDatasetAsCString[] = "ADCO 056234673197 L";
 		std::vector<uint8_t> expectedFirstDatasetInFrame(firstDatasetAsCString, firstDatasetAsCString+strlen(firstDatasetAsCString));
 		if (stub.decodedDatasetList[0] != expectedFirstDatasetInFrame) {
 			FAILF("Unexpected first dataset in first frame:\nGot:      %s\nExpected: %s\n", vectorToHexString(stub.decodedDatasetList[0]).c_str(), vectorToHexString(expectedFirstDatasetInFrame).c_str());
@@ -261,7 +261,7 @@ TEST(TicDatasetExtractor_tests, Chunked_sample_unframe_dsextract_standard_TIC) {
 		if (stub.decodedDatasetList.size() != expectedTotalDatasetCount) { 
 			FAILF("When using chunk size %zu: Wrong dataset count: %zu, expected %zu\nDatasets received:\n%s", chunkSize, stub.decodedDatasetList.size(), expectedTotalDatasetCount, stub.toString().c_str());
 		}
-		char firstDatasetAsCString[] = "ADSC\t064468368739\tJ";
+		char firstDatasetAsCString[] = "ADSC\t064468368739\tM";
 		std::vector<uint8_t> expectedFirstDatasetInFrame(firstDatasetAsCString, firstDatasetAsCString+strlen(firstDatasetAsCString));
 		if (stub.decodedDatasetList[0] != expectedFirstDatasetInFrame) {
 			FAILF("Unexpected first dataset in first frame:\nGot:      %s\nExpected: %s\n", vectorToHexString(stub.decodedDatasetList[0]).c_str(), vectorToHexString(expectedFirstDatasetInFrame).c_str());
