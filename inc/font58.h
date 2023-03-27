85420de42907ef6c6b0490d8d5397ce51a90b6e9
lains <383502+lains@users.noreply.github.com>
2023-03-27 19:17:22 +0200

    Adding big (60x120 px) font

---
+++ inc/font58.h
@@ -0,0 +1,8 @@
+/* Automatically generated font data using FreeType2 */
+/* generated from Inconsolata-Regular.ttf */
+#include <stdint.h>
+
+#define CHAR_WIDTH 60	/* width */
+#define CHAR_HEIGHT 120 /* height */
+
+const uint8_t* get_font58_ptr(const char asciiChar);
