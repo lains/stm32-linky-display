a38b76ca76b01fa1a099667a6e462482591c1cd0
lains <383502+lains@users.noreply.github.com>
2024-01-24 10:37:15 +0100

    Cleaning-up unused files

--- inc/main.h
+++ inc/main.h
@@ -1,22 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    main.h
-  * @brief   Header for main.c module
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
-
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
 #define __MAIN_H
 
@@ -25,32 +6,9 @@
 #include <string.h>
 #include "stm32f7xx_hal.h"
 #include "stm32f769i_discovery.h"
+//#include "stm32f769i_discovery_ts.h"
 #include "stm32f769i_discovery_lcd.h"
 #include "stm32f769i_discovery_qspi.h"
-#include "stm32f769i_discovery_ts.h"
-#include "stm32f769i_discovery_audio.h"
-#include "stm32f769i_discovery_sd.h"
-
-/* Exported types ------------------------------------------------------------*/
-typedef struct
-{
-  void   (*DemoFunc)(void);
-  uint8_t DemoName[50]; 
-  uint32_t DemoIndex;
-}BSP_DemoTypedef;
-
-typedef enum {
-  AUDIO_ERROR_NONE = 0,
-  AUDIO_ERROR_NOTREADY,
-  AUDIO_ERROR_IO,
-  AUDIO_ERROR_EOF,
-}AUDIO_ErrorTypeDef;
-
-/* Exported variables --------------------------------------------------------*/
-extern const unsigned char stlogo[];
-extern __IO uint32_t SdmmcTest;
-extern __IO uint32_t SdramTest;
-/* Exported constants --------------------------------------------------------*/
 
 /* LCD Frame buffer start address : starts at beginning of SDRAM */  
 #define ARBG8888_BYTE_PER_PIXEL   4
@@ -61,42 +19,7 @@ extern __IO uint32_t SdramTest;
 #define AUDIO_SRC_FILE_ADDRESS       0x08080000   /* Audio file address in flash */
 #define AUDIO_FILE_SIZE              0x80000
 
-/* Exported macro ------------------------------------------------------------*/
-#define COUNT_OF_EXAMPLE(x)    (sizeof(x)/sizeof(BSP_DemoTypedef))
-
-#ifdef USE_FULL_ASSERT
-/* Assert activated */
-#define BSP_TEST_APPLI_ASSERT(__error_condition__)    do { if(__error_condition__) \
-                                                           {  while(1);  \
-                                                           } \
-                                                          } while(0)
-#else
-/* Assert not activated : macro has no effect */
-#define BSP_TEST_APPLI_ASSERT(__error_condition__)    do { if(__error_condition__) \
-                                                           {  ;  \
-                                                           } \
-                                                         } while(0)
-#endif /* USE_FULL_ASSERT */
-
-/* Exported functions ------------------------------------------------------- */
-uint8_t TouchScreen_GetTouchPosition(void);
-void Touchscreen_DrawBackground_Circles(uint8_t state);
-void     Touchscreen_demo1 (void);
-void     Touchscreen_demo2 (void);
-void AudioPlay_demo (void);
-void AudioLoopback_demo (void);
-uint8_t AUDIO_Process(void);
-void LCD_demo (void);
-void SD_demo (void);
-void QSPI_demo (void);
-void EEPROM_demo (void);
-uint8_t CheckForUserInput(void);
-void Toggle_Leds(void);
 void Error_Handler(void);
-void SDRAM_demo(void);
-void SDRAM_DMA_demo (void);
-void Log_demo(void);
+void OnError_Handler(uint32_t condition);
 
 #endif /* __MAIN_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
