d8d5c92ed0eead545974509a422f94729ceda0ce
lains <383502+lains@users.noreply.github.com>
2024-02-20 18:24:45 +0100

    Allowing board-dependent USART definitions for TIC and debug

--- inc/hal/Stm32SerialDriver.h
+++ inc/hal/Stm32SerialDriver.h
@@ -9,6 +9,79 @@
 #include <string>
 #endif
 
+/* Definition for the USART used to receive the TIC signal */
+#ifdef USE_STM32469I_DISCOVERY
+/**
+ * USART6 GPIO Configuration
+ * PC6     ------> USART6_TX
+ * PC7     ------> USART6_RX
+ */
+/* Definition for USART6 HAL functions */
+#define USART6_CLK_ENABLE()              __HAL_RCC_USART6_CLK_ENABLE()
+#define USART6_CLK_DISABLE()             __HAL_RCC_USART6_CLK_DISABLE()
+#define USART6_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOC_CLK_ENABLE()
+#define USART6_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOC_CLK_ENABLE()
+#define USART6_FORCE_RESET()             __HAL_RCC_USART6_FORCE_RESET()
+#define USART6_RELEASE_RESET()           __HAL_RCC_USART6_RELEASE_RESET()
+#define USART6_TX_PIN                    GPIO_PIN_6
+#define USART6_TX_GPIO_PORT              GPIOC
+#define USART6_TX_AF                     GPIO_AF8_USART6
+#define USART6_RX_PIN                    GPIO_PIN_7
+#define USART6_RX_GPIO_PORT              GPIOC
+#define USART6_RX_AF                     GPIO_AF8_USART6
+
+#define USART_TIC                        USART6
+#define USART_TIC_CLK_ENABLE()           USART6_CLK_ENABLE()
+#define USART_TIC_CLK_DISABLE()          USART6_CLK_DISABLE()
+#define USART_TIC_RX_GPIO_CLK_ENABLE()   USART6_RX_GPIO_CLK_ENABLE()
+#define USART_TIC_TX_GPIO_CLK_ENABLE()   USART6_TX_GPIO_CLK_ENABLE()
+#define USART_TIC_FORCE_RESET()          USART6_FORCE_RESET()
+#define USART_TIC_RELEASE_RESET()        USART6_RELEASE_RESET()
+#define USART_TIC_TX_PIN                 USART6_TX_PIN
+#define USART_TIC_TX_GPIO_PORT           USART6_TX_GPIO_PORT
+#define USART_TIC_TX_AF                  USART6_TX_AF
+#define USART_TIC_RX_PIN                 USART6_RX_PIN
+#define USART_TIC_RX_GPIO_PORT           USART6_RX_GPIO_PORT
+#define USART_TIC_RX_AF                  USART6_RX_AF
+#define USART_TIC_IRQn                   USART6_IRQn
+#endif
+
+/* Definition for the USART forwarded to ST-Link's virtual com port */
+#ifdef USE_STM32469I_DISCOVERY
+/**
+ * USART3 GPIO Configuration
+ * PD8     ------> USART3_TX
+ * PD9     ------> USART3_RX
+ */
+/* Definition for USART3 HAL functions */
+#define USART3_CLK_ENABLE()              __HAL_RCC_USART3_CLK_ENABLE()
+#define USART3_CLK_DISABLE()             __HAL_RCC_USART3_CLK_DISABLE()
+#define USART3_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USART3_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USART3_FORCE_RESET()             __HAL_RCC_USART3_FORCE_RESET()
+#define USART3_RELEASE_RESET()           __HAL_RCC_USART3_RELEASE_RESET()
+#define USART3_TX_PIN                    GPIO_PIN_8
+#define USART3_TX_GPIO_PORT              GPIOD
+#define USART3_TX_AF                     GPIO_AF7_USART3
+#define USART3_RX_PIN                    GPIO_PIN_9
+#define USART3_RX_GPIO_PORT              GPIOD
+#define USART3_RX_AF                     GPIO_AF7_USART3
+
+#define USART_DBG                        USART3
+#define USART_DBG_CLK_ENABLE()           USART3_CLK_ENABLE()
+#define USART_DBG_CLK_DISABLE()          USART3_CLK_DISABLE()
+#define USART_DBG_RX_GPIO_CLK_ENABLE()   USART3_RX_GPIO_CLK_ENABLE()
+#define USART_DBG_TX_GPIO_CLK_ENABLE()   USART3_TX_GPIO_CLK_ENABLE()
+#define USART_DBG_FORCE_RESET()          USART3_FORCE_RESET()
+#define USART_DBG_RELEASE_RESET()        USART3_RELEASE_RESET()
+#define USART_DBG_TX_PIN                 USART3_TX_PIN
+#define USART_DBG_TX_GPIO_PORT           USART3_TX_GPIO_PORT
+#define USART_DBG_TX_AF                  USART3_TX_AF
+#define USART_DBG_RX_PIN                 USART3_RX_PIN
+#define USART_DBG_RX_GPIO_PORT           USART3_RX_GPIO_PORT
+#define USART_DBG_RX_AF                  USART3_RX_AF
+#endif
+
 extern "C" {
 UART_HandleTypeDef* get_huart6(void);   // C-linkage exported getter for huart6 handler
 }
